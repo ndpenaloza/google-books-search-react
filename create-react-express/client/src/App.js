e6ac3464f98850a0172954c8168c699e00784e0b
NDPenaloza <ndpenaloza@gmail.com>
2021-03-08 00:46:43 -0800

    did some routing working in app.js

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Search}/>
+          <Route exact path='/saved' component={Saved}/>
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
