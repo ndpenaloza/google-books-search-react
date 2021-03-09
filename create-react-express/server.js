6b26f7a4a3adfcc643f5e649cef6746c72d69b01
NDPenaloza <ndpenaloza@gmail.com>
2021-03-08 22:45:16 -0800

    added mongoose and mongodb environment variable

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,12 +1,17 @@
+// Dependencies
 const express = require("express");
+const mongoose = require('mongoose');
 const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-// Define middleware here
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+// Static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -18,6 +23,17 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/google-books-search',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
